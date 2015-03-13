4ce4ebe987da7e371569aeb372c8056352b2b9c7
Bryce Boe <bbzbryce@gmail.com>
2015-03-12 21:49:53 -0700

    Force SSL on production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    Rails.env.production?
+  end
 end
