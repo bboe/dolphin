0a288ec1ed528ec0ece48925c784691c3d15d2d3
Bryce Boe <bbzbryce@gmail.com>
2016-08-08 13:48:41 -0700

    Response with 406 instead of 500 for MissingTemplate.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   force_ssl if: :ssl_configured?
 
+  rescue_from ActionView::MissingTemplate do
+    # RAILS5TODO: Remove this rescue when upgrading to rails5.
+    raise unless Rails.env.production?
+    render nothing: true, status: :not_acceptable
+  end
+
   def ssl_configured?
     Rails.env.production?
   end
