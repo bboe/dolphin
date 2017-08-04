22a9419728c9118c645623e9c3415115eb7a7fb0
Bryce Boe <bbzbryce@gmail.com>
2017-08-04 11:56:36 -0700

    Follow best practices in model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,26 +1,63 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  test 'user fails to save with duplicate email' do
+    create_user(uid: '01').save!
 
-  test 'presence of fields' do
-    assert (user = User.new).invalid?, 'missing required fields'
-    assert_equal [:email, :name, :image_url, :provider, :uid], user.errors.keys
+    user = create_user(uid: '02')
+    assert_predicate user, :invalid?
+    assert_equal({email: ["has already been taken"]}, user.errors.messages)
   end
 
-  test 'duplicate email' do
-    assert create_user(uid: '01').save
-    assert (user = create_user(uid: '02')).invalid?, 'email must be unique'
-    assert_equal [:email], user.errors.keys
-  end
-
-  test 'duplicate provider and uid' do
+  test 'user fails to save with duplicate provider and uid' do
     assert create_user(email: 'user1@test').save
     assert (user = create_user(email: 'user2@test')).invalid?,
            'provider and uid must be unique'
     assert_equal [:uid], user.errors.keys
   end
 
-  test 'valid user' do
-    assert create_user.valid?
+  test 'user fails to save without email' do
+    [nil, ''].each do |value|
+      user = create_user(email: value)
+      assert_predicate user, :invalid?
+      assert_equal({email: ["can't be blank"]}, user.errors.messages)
+    end
+  end
+
+  test 'user fails to save without image_url' do
+    [nil, ''].each do |value|
+      user = create_user(image_url: value)
+      assert_predicate user, :invalid?
+      assert_equal({image_url: ["can't be blank"]}, user.errors.messages)
+    end
+  end
+
+  test 'user fails to save without name' do
+    [nil, ''].each do |value|
+      user = create_user(name: value)
+      assert_predicate user, :invalid?
+      assert_equal({name: ["can't be blank"]}, user.errors.messages)
+    end
+  end
+
+  test 'user fails to save without provider' do
+    [nil, ''].each do |value|
+      user = create_user(provider: value)
+      assert_predicate user, :invalid?
+      assert_equal({provider: ["can't be blank"]}, user.errors.messages)
+    end
+  end
+
+  test 'user fails to save without uid' do
+    [nil, ''].each do |value|
+      user = create_user(uid: value)
+      assert_predicate user, :invalid?
+      assert_equal({uid: ["can't be blank"]}, user.errors.messages)
+    end
+  end
+
+
+  test 'user saves when valid' do
+    assert_predicate create_user, :valid?
   end
 end
