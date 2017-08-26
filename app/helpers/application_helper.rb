3831e03d9fc67df43483fc8330d529b9bc207b36
Bryce Boe <bbzbryce@gmail.com>
2017-08-25 22:15:13 -0700

    Fix dolpin -> dolphin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   STOCK_IMAGE_URL = 'https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg?sz=50'
 
   def display_user(user)
-    path = user.image_url == STOCK_IMAGE_URL ? image_path('dolpin.png') : user.image_url
+    path = user.image_url == STOCK_IMAGE_URL ? image_path('dolphin.png') : user.image_url
     "#{image_tag(path)} #{user.name}"
   end
 end
