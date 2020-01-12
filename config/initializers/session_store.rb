fc2015ad0175ca2b462bd26c153f7aade6c32df5
Bryce Boe <bbzbryce@gmail.com>
2020-01-11 23:08:26 -0800

    Update session_store configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_dolphin_session'
+Rails.application.config.session_store :cookie_store, domain: "dolphinfolio.com", expire_after: 9999.days, key: '_dolphin_session'
