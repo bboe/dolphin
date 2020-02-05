92d9bd072f91418da6ca5769debab529db2be6a9
Bryce Boe <bbzbryce@gmail.com>
2020-02-04 21:41:58 -0800

    Fix omniauth issue in non-production mode

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,8 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, domain: "dolphinfolio.com", expire_after: 9999.days, key: '_dolphin_session'
+if Rails.env.production?
+    Rails.application.config.session_store :cookie_store, domain: "dolphinfolio.com", expire_after: 9999.days, key: '_dolphin_session'
+else
+    Rails.application.config.session_store :cookie_store, expire_after: 9999.days, key: '_dolphin_session'
+end
